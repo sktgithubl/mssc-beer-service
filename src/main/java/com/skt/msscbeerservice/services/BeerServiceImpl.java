37bd4dcd4e55057177b62f51e943f98a8c4b0146
sktgithubl <skt.sipun123@gmail.com>
2021-09-09 12:05:38 +0530

    beer service added

---
+++ src/main/java/com/skt/msscbeerservice/services/BeerServiceImpl.java
@@ -0,0 +1,43 @@
+package com.skt.msscbeerservice.services;
+
+import com.skt.msscbeerservice.domain.Beer;
+import com.skt.msscbeerservice.repository.BeerRepository;
+import com.skt.msscbeerservice.web.controller.NotFoundException;
+import com.skt.msscbeerservice.web.mappers.BeerMapper;
+import com.skt.msscbeerservice.web.model.BeerDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public BeerDto getById(UUID beerId) {
+        return beerMapper.beerToBeerDto(
+                beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
+        );
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    }
+
+    @Override
+    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
+        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+
+        beer.setBeerName(beerDto.getBeerName());
+        beer.setBeerStyle(beerDto.getBeerStyle().name());
+        beer.setPrice(beerDto.getPrice());
+        beer.setUpc(beerDto.getUpc());
+
+        return beerMapper.beerToBeerDto(beerRepository.save(beer));
+    }
+}
--- src/test/java/com/skt/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/skt/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,19 +1,21 @@
 package com.skt.msscbeerservice.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.skt.msscbeerservice.domain.Beer;
+import com.skt.msscbeerservice.services.BeerService;
 import com.skt.msscbeerservice.web.model.BeerDto;
 import com.skt.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,8 +28,13 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerService beerService;
+
     @Test
     void getBeerById() throws Exception {
+
+        Mockito.when(beerService.getById(any(UUID.class))).thenReturn(getCompleteBeerDto());
 
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
@@ -37,6 +44,7 @@ class BeerControllerTest {
     void saveNewBeer() throws Exception {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+        Mockito.when(beerService.saveNewBeer(any(BeerDto.class))).thenReturn(getCompleteBeerDto());
 
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON).content(beerDtoJson))
@@ -47,6 +55,7 @@ class BeerControllerTest {
     void updateBeerById() throws Exception {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+        Mockito.when(beerService.updateBeer(any(UUID.class), any(BeerDto.class))).thenReturn(getCompleteBeerDto());
 
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON).content(beerDtoJson))
@@ -54,6 +63,15 @@ class BeerControllerTest {
     }
 
     BeerDto getValidBeerDto() {
+        return BeerDto.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(123123123123L)
+                .build();
+    }
+
+    BeerDto getCompleteBeerDto() {
         return BeerDto.builder()
                 .beerName("My Beer")
                 .beerStyle(BeerStyleEnum.ALE)
